a9534aeb207407f54b88c1ab0b7f8d61a53a32b2
Ruqaiya <ruqaiya.sattar345@gmail.com>
2023-01-30 12:59:32 +0500

    prototype1

---
+++ project-a/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mboathoscope/screens/RolePage.dart';
+import 'package:mboathoscope/screens/StartPage.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      // title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: const StartPage(),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '': (context) => const StartPage(),
+        '/rolepage': (context) => const RolePage(),
+      },
+    ),
+  );
+}
+
---
+++ project-a/lib/screens/HomePage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     appBar: AppBar(
+       title: const Text("Home Page"),
+     ),
+    );
+  }
+}
