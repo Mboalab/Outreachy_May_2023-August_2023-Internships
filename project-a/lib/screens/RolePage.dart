a90ae9cdbd70010b3e1bcab39b211824308b6545
logickoder <chukwudumebiorazulike@gmail.com>
2023-03-13 08:06:21 +0100

    fix: overflow in RolePage.dart

--- project-a/lib/screens/RolePage.dart
+++ project-a/lib/screens/RolePage.dart
@@ -1,78 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:mboathoscope/CustomButton.dart';
+
+import '../CustomButton.dart';
+import '../main.dart';
 
 class RolePage extends StatelessWidget {
   const RolePage({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
-      backgroundColor: const Color(0xffF3F7FF),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 100,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 26,right: 26, top: 87),
-            child: Image.asset(
-              'assets/images/img_role.png',
-              height: 280,
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 23, right: 23),
-            child: Text(
-              'Please select what your role is',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
+      backgroundColor: theme.colorScheme.background,
+      body: SizedBox.expand(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/img_role.png',
+                height: 280,
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 55, right: 38, left: 38),
-            child: Row(
-              children: const [
-                 CustomButton(
-                  txt: 'Transmitter',
-                ),
-                 SizedBox(
-                  width: 60,
-                ),
-                 CustomButton(
-                  txt: 'Receiver',
-                )
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 300, top: 30),
-            child: GestureDetector(
-              onTap: (){
-                Navigator.pushNamed(context,'/homepage');
-              },
-              child: RichText(
-                text: const TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "Next ",
-                      style: TextStyle(color: Colors.black)
-                    ),
-                    WidgetSpan(
-                      child: Icon(Icons.arrow_forward_ios, size: 14),
-                    ),
-                  ],
+              const SizedBox(height: 16),
+              Text(
+                'Please select what your role is',
+                style: theme.textTheme.headlineSmall?.copyWith(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
+              const SizedBox(height: 32),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  CustomButton(
+                    txt: 'Transmitter',
+                  ),
+                  CustomButton(
+                    txt: 'Receiver',
+                  )
+                ],
+              ),
+              const SizedBox(height: 32),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton.icon(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: () => Navigator.pushNamed(
+                    context,
+                    AppRoutes.homePage,
+                  ),
+                  icon: const Text("Next"),
+                  label: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 14,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
