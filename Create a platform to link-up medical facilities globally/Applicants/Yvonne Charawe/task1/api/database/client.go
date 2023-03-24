c8f804846ef0c1c901a319b0490cc579bba414e9
Charawey-X <charawey@gmail.com>
2023-03-25 01:15:23 +0300

    connect to the database via GORM helpers

---
+++ Create a platform to link-up medical facilities globally/Applicants/Yvonne Charawe/task1/api/database/client.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"golang-crud-rest-api/entities"
+	"log"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+//Instance of the database
+var Instance *gorm.DB
+//Error if connection to database fails
+var err error
+
+//Connect attempts to connect to the database via GORM helpers
+func Connect(connectionString string) {
+	Instance, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+		panic("Cannot connect to DB")
+	}
+	log.Println("Connected to Database...")
+}
+func Migrate(){	
+	Instance.AutoMigrate(&entities.Hospital{})
+	log.Println("Database Migration Completed...")
+}
