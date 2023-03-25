2d5961f42700bf17cd1150be26bad04c6e713ae6
Charawey-X <charawey@gmail.com>
2023-03-25 17:48:24 +0300

    update routes and readme files

---
+++ Create a platform to link-up medical facilities globally/Applicants/Yvonne Charawe/task1/README.md
@@ -0,0 +1,12 @@
+# Data Collection Tool
+
+## API 
+
+- This is the application programming interface that enables addition, update and view of hospitals.
+- The details on this block of the data collection tool are in the [api folder](api/)
+
+## DESIGN
+- This is the design of the user-interface where users will register and update hospital details.
+- All files and links on the design are in the [design folder](design/)
+
+## WEBSITE
--- Create a platform to link-up medical facilities globally/Applicants/Yvonne Charawe/task1/api/router.go
+++ Create a platform to link-up medical facilities globally/Applicants/Yvonne Charawe/task1/api/router.go
@@ -6,12 +6,12 @@ import (
 )
 
 func HospitalRoutes(router *mux.Router) {
-	router.HandleFunc("/api/hospitals", controllers.GetAllHospitals).Methods("GET");
-	router.HandleFunc("/api/hospitals/{id}", controllers.GetHospitalById).Methods("GET");
-	router.HandleFunc("/api/hospitals/names/{name}", controllers.GetHospitalByName).Methods("GET");
-	router.HandleFunc("/api/hospitals/countries/{country}", controllers.GetHospitalByCountry).Methods("GET");
-	router.HandleFunc("/api/hospitals/services/{service}", controllers.GetHospitalByService).Methods("GET");
-	router.HandleFunc("/api/hospitals/new", controllers.AddHospital).Methods("POST");
-	router.HandleFunc("/api/hospitals/{id}", controllers.UpdateHospital).Methods("PUT");
-	router.HandleFunc("/api/hospitals/delete/{id}", controllers.DeleteHospital).Methods("DELETE");
+	router.HandleFunc("/api/v1/hospitals", controllers.GetAllHospitals).Methods("GET");
+	router.HandleFunc("/api/v1/hospitals/{id}", controllers.GetHospitalById).Methods("GET");
+	router.HandleFunc("/api/v1/hospitals/names/{name}", controllers.GetHospitalByName).Methods("GET");
+	router.HandleFunc("/api/v1/hospitals/countries/{country}", controllers.GetHospitalByCountry).Methods("GET");
+	router.HandleFunc("/api/v1/hospitals/services/{service}", controllers.GetHospitalByService).Methods("GET");
+	router.HandleFunc("/api/v1/hospitals/new", controllers.AddHospital).Methods("POST");
+	router.HandleFunc("/api/v1/hospitals/{id}", controllers.UpdateHospital).Methods("PUT");
+	router.HandleFunc("/api/v1/hospitals/delete/{id}", controllers.DeleteHospital).Methods("DELETE");
 }
