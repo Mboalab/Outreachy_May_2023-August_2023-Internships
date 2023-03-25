5b8aa93e960f2d8b02627f2c0540af300bee5cd9
Charawey-X <charawey@gmail.com>
2023-03-25 11:25:38 +0300

    test endpoint routes

--- Create a platform to link-up medical facilities globally/Applicants/Yvonne Charawe/task1/api/controllers/hospitalcontroller.go
+++ Create a platform to link-up medical facilities globally/Applicants/Yvonne Charawe/task1/api/controllers/hospitalcontroller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"golang-crud-rest-api/database"
 	"golang-crud-rest-api/entities"
 	"net/http"
@@ -40,6 +41,47 @@ func GetHospitalById(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(hospital);
 }
 
+//GetHospitalByName returns a hospital in the database with the specified name (READ)
+func GetHospitalByName(w http.ResponseWriter, r *http.Request) {
+	hospitalName := mux.Vars(r)["name"];
+	var hospitals []entities.Hospital;
+	hospitalName = fmt.Sprintf("%%%s%%", hospitalName);
+	database.Instance.Where("name LIKE ?", hospitalName).Find(&hospitals);
+	if len(hospitals) == 0 {
+		json.NewEncoder(w).Encode("No Hospital Found!");
+		return
+	}
+	w.Header().Set("Content-Type", "application/json");
+	json.NewEncoder(w).Encode(hospitals);
+}
+
+//GetHospitalByCountry returns a hospital in the database with the specified country (READ)
+func GetHospitalByCountry(w http.ResponseWriter, r *http.Request) {
+	hospitalCountry := mux.Vars(r)["country"];
+	var hospitals []entities.Hospital;
+	hospitalCountry = fmt.Sprintf("%s%%", hospitalCountry);
+	database.Instance.Where("country LIKE ?", hospitalCountry).Find(&hospitals);
+	if len(hospitals) == 0 {
+		json.NewEncoder(w).Encode("No Hospital Found!");
+		return
+	}
+	w.Header().Set("Content-Type", "application/json");
+	json.NewEncoder(w).Encode(hospitals);
+}
+
+//GetHospitalByService returns a hospital in the database with the specified service (READ)
+func GetHospitalByService(w http.ResponseWriter, r *http.Request) {
+	hospitalService := mux.Vars(r)["service"];
+	var hospitals []entities.Hospital;
+	hospitalService = fmt.Sprintf("%%%s%%", hospitalService);
+	database.Instance.Where("services LIKE ?", hospitalService).Find(&hospitals);
+	if len(hospitals) == 0 {
+		json.NewEncoder(w).Encode("No Hospital Found!");
+		return
+	}
+	w.Header().Set("Content-Type", "application/json");
+	json.NewEncoder(w).Encode(hospitals);
+}
 
 //GetAllHospitals returns all hospitals in the database (READ)
 
--- Create a platform to link-up medical facilities globally/Applicants/Yvonne Charawe/task1/api/router.go
+++ Create a platform to link-up medical facilities globally/Applicants/Yvonne Charawe/task1/api/router.go
@@ -8,6 +8,9 @@ import (
 func HospitalRoutes(router *mux.Router) {
 	router.HandleFunc("/api/hospitals", controllers.GetAllHospitals).Methods("GET");
 	router.HandleFunc("/api/hospitals/{id}", controllers.GetHospitalById).Methods("GET");
+	router.HandleFunc("/api/hospitals/names/{name}", controllers.GetHospitalByName).Methods("GET");
+	router.HandleFunc("/api/hospitals/countries/{country}", controllers.GetHospitalByCountry).Methods("GET");
+	router.HandleFunc("/api/hospitals/services/{service}", controllers.GetHospitalByService).Methods("GET");
 	router.HandleFunc("/api/hospitals/new", controllers.AddHospital).Methods("POST");
 	router.HandleFunc("/api/hospitals/{id}", controllers.UpdateHospital).Methods("PUT");
 	router.HandleFunc("/api/hospitals/delete/{id}", controllers.DeleteHospital).Methods("DELETE");
